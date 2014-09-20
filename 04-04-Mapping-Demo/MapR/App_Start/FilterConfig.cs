21fd216ed92c2d969379b36f3cb0a5bef8ab59bb
bradygaster <brady.gaster@gmail.com>
2014-09-20 00:24:52 -0700

    added mapping demo

---
+++ 04-04-Mapping-Demo/MapR/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MapR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

485bad6d1e006e1a495fb92d72e5ef8a070fab89
Jon Galloway <jongalloway@gmail.com>
2014-09-21 00:26:42 -0700

    Added 01-02-Persistent-Connection demo

---
+++ 01-02-Persistent-Connection/PersistentConnection/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+using PersistentConnectionDemo.Features.Authorization;
+using PersistentConnectionDemo.Features.SamplePersistentConnection;
+
+[assembly: OwinStartupAttribute(typeof(PersistentConnectionDemo.Startup))]
+namespace PersistentConnectionDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+            app.MapSignalR<DemoPersistentConnection>("/Connections/DemoPersistentConnection");
+            app.MapSignalR<AuthorizationPersistentConnection>("/Connections/AuthorizationPersistentConnection");
+
+            app.Map("/EnableDetailedErrors", map =>
+            {
+                var hubConfiguration = new HubConfiguration
+                {
+                    EnableDetailedErrors = true
+                };
+
+                map.MapSignalR(hubConfiguration);
+            });
+        }
+    }
+}

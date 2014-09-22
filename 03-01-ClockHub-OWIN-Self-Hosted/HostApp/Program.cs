aae803685094b47c574aac2b27305feb0880ad96
bradygaster <brady.gaster@gmail.com>
2014-09-21 21:00:07 -0700

    added self-hosted demo

---
+++ 03-01-ClockHub-OWIN-Self-Hosted/HostApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:8080";
+            using(WebApp.Start(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 03-01-ClockHub-OWIN-Self-Hosted/HostApp/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Cors;
+
+[assembly: OwinStartup(typeof(HostApp.Startup))]
+
+namespace HostApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}

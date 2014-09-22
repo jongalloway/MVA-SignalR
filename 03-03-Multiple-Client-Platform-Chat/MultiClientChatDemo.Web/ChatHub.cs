2d32a4e37a952a3a5033b0e610cde84e81f28e94
bradygaster <brady.gaster@gmail.com>
2014-09-22 15:36:26 -0700

    added new multi-client chat

---
+++ 03-03-Multiple-Client-Platform-Chat/MultiClientChatDemo.Web/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace MultiClientChatDemo.Web
+{
+    [HubName("chat")]
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string msg)
+        {
+            Clients.All.receiveMessage(msg);
+        }
+    }
+}

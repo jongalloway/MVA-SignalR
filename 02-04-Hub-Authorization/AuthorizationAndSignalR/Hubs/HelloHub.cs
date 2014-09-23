71b47b6c25cee31459c650b5513f50b324a97da0
bradygaster <brady.gaster@gmail.com>
2014-09-22 23:41:38 -0700

    added authorization demo

---
+++ 02-04-Hub-Authorization/AuthorizationAndSignalR/Hubs/HelloHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace AuthorizationAndSignalR.Hubs
+{
+    //[Authorize]
+    public class HelloHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
